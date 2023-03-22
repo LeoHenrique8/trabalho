07752517ece1becd101ca6b14ce66d44c8a5b752
Leonardo Henrique <theexpendablesgames@gmail.com>
2023-03-22 19:27:46 -0300

    primeiro commit

---
+++ node.js
@@ -0,0 +1,11 @@
+const pessoas = [
+    {nome: "João", idade: 40},
+    {nome: "Irene", idade: 30},
+    {nome: "Paulo", idade: 21},
+    {nome: "Ana", idade: 23},
+    {nome: "Carlos", idade: 17},
+]
+
+for (p of pessoas){
+    console.log(p)
+}
